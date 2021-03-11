d470d443880665fdda7b5766872a9f5443e19847
Ashton <52977938+AshtonMcEntarffer@users.noreply.github.com>
2021-03-10 21:23:02 -0500

    livereload works on sending message

--- static/js/app.js
+++ static/js/app.js
@@ -5,26 +5,29 @@ let chatInput = $("#chat-input");
 let chatButton = $("#btn-send");
 let userList = $("#user-list");
 let messageList = $("#messages");
-					function updateUserList() {
-						$.getJSON("api/v1/user/", function (data) {
-							userList.children(".user").remove();
-							for (let i = 0; i < data.length; i++) {
-								const userItem = `<a class="list-group-item user">${data[i]["username"]}</a>`;
-								$(userItem).appendTo("#user-list");
-							}
-							$(".user").click(function () {
-								userList.children(".active").removeClass("active");
-								let selected = event.target;
-								$(selected).addClass("active");
-								setCurrentRecipient(selected.text);
-							});
-						});
-					}
-					function drawMessage(message) {
-						let position = "left";
-						const date = new Date(message.timestamp);
-						if (message.user === currentUser) position = "right";
-						const messageItem = `
+function updateUserList() {
+	$.getJSON("api/v1/user/", function (data) {
+		userList.children(".user").remove();
+		for (let i = 0; i < data.length; i++) {
+			const userItem = `<a class="list-group-item user">${data[i]["username"]}</a>`;
+			$(userItem).appendTo("#user-list");
+		}
+		$(".user").click(function () {
+			userList.children(".active").removeClass("active");
+			let selected = event.target;
+			$(selected).addClass("active");
+			setCurrentRecipient(selected.text);
+		});
+	});
+}
+
+var currentMessages = []
+
+function drawMessage(message) {
+	let position = "left";
+	const date = new Date(message.timestamp);
+	if (message.user === currentUser) position = "right";
+	var messageItem = `
             <li class="message ${position}">
                 <div class="avatar">${message.user}</div>
                     <div class="text_wrapper">
@@ -33,81 +36,82 @@ let messageList = $("#messages");
                     </div>
                 </div>
             </li>`;
-						$(messageItem).appendTo("#messages");
-					}
+	$(messageItem).appendTo("#messages")
+}
 
-					function getConversation(recipient) {
-						$.getJSON(`/api/v1/message/?target=${recipient}`, function (data) {
-							messageList.children(".message").remove();
-							for (let i = data["results"].length - 1; i >= 0; i--) {
-								drawMessage(data["results"][i]);
-							}
-							messageList.animate({
-								scrollTop: messageList.prop("scrollHeight"),
-							});
-						});
-					}
+function getConversation(recipient) {
+	$.getJSON(`/api/v1/message/?target=${recipient}`, function (data) {
+		messageList.children(".message").remove();
+		for (let i = data["results"].length - 1; i >= 0; i--) {
+			drawMessage(data["results"][i]);
+		}
+		messageList.animate({
+			scrollTop: messageList.prop("scrollHeight"),
+		});
+	});
+}
 
-					function getMessageById(message) {
-						id = JSON.parse(message).message;
-						$.getJSON(`/api/v1/message/${id}/`, function (data) {
-							if (
-								data.user === currentRecipient ||
-								(data.recipient === currentRecipient &&
-									data.user == currentUser)
-							) {
-								drawMessage(data);
-							}
-							messageList.animate({
-								scrollTop: messageList.prop("scrollHeight"),
-							});
-						});
-					}
-					function sendMessage(recipient, body) {
-						$.post("/api/v1/message/", {
-							recipient: recipient,
-							body: body,
-						}).fail(function () {});
-					}
+function getMessageById(message) {
+	id = JSON.parse(message).message;
+	$.getJSON(`/api/v1/message/${id}/`, function (data) {
+		if (
+			data.user === currentRecipient ||
+			(data.recipient === currentRecipient &&
+				data.user == currentUser)
+		) {
+			drawMessage(data);
+		}
+		messageList.animate({
+			scrollTop: messageList.prop("scrollHeight"),
+		});
+	});
+}
+function sendMessage(recipient, body) {
+	$.post("/api/v1/message/", {
+		recipient: recipient,
+		body: body,
+	}).fail(function () { });
+	getConversation(recipient);
+}
 
-					function setCurrentRecipient(username) {
-						currentRecipient = username;
-						getConversation(currentRecipient);
-						enableInput();
-					}
+function setCurrentRecipient(username) {
+	currentRecipient = username;
+	getConversation(currentRecipient);
+	enableInput();
+}
 
-					function enableInput() {
-						chatInput.prop("disabled", false);
-						chatButton.prop("disabled", false);
-						chatInput.focus();
-					}
+function enableInput() {
+	chatInput.prop("disabled", false);
+	chatButton.prop("disabled", false);
+	chatInput.focus();
+}
 
-					function disableInput() {
-						chatInput.prop("disabled", true);
-						chatButton.prop("disabled", true);
-					}
+function disableInput() {
+	chatInput.prop("disabled", true);
+	chatButton.prop("disabled", true);
+}
 
-					$(document).ready(function () {
-						updateUserList();
-						disableInput();
+$(document).ready(function () {
+	updateUserList();
+	disableInput();
 
-						//    let socket = new WebSocket(`wss://127.0.0.1:8000/?session_key=${sessionKey}`);
-						var socket = new WebSocket(
-							"wss://" + window.location.host + "/wss?session_key=${sessionKey}"
-						);
+	//    let socket = new WebSocket(`wss://127.0.0.1:8000/?session_key=${sessionKey}`);
+	var socket = new WebSocket(
+		"wss://" + window.location.host + "/wss?session_key=${sessionKey}"
+	);
 
-						chatInput.keypress(function (e) {
-							if (e.keyCode == 13) chatButton.click();
-						});
+	chatInput.keypress(function (e) {
+		if (e.keyCode == 13) chatButton.click();
+	});
 
-						chatButton.click(function () {
-							if (chatInput.val().length > 0) {
-								sendMessage(currentRecipient, chatInput.val());
-								chatInput.val("");
-							}
-						});
+	chatButton.click(function () {
+		if (chatInput.val().length > 0) {
+			sendMessage(currentRecipient, chatInput.val());
+			chatInput.val("");
+		}
+	});
 
-						socket.onmessage = function (e) {
-							getMessageById(e.data);
-						};
-					});
+	socket.onmessage = function (e) {
+		getMessageById(e.data);
+	};
+});
